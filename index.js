18168994d4aec2a5524e95693b68590bd9b6cd2c
Patricio de la Guardia <pdelaguardia87@gmail.com>
2018-01-12 10:17:17 -0500

    Add google client id keys and secrets

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
