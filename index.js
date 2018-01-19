0d4328a58478d24d1893bb8a554062ebe1644d7e
Patricio de la Guardia <pdelaguardia87@gmail.com>
2018-01-19 11:15:31 -0500

    Hook up app with mongoose

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
