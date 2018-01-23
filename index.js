fe526d3ccfb56b940d7988483006d32640b17cd7
Patricio de la Guardia <pdelaguardia87@gmail.com>
2018-01-23 10:46:13 -0500

    Add user model

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./services/passport");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 2592000000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
